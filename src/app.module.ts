3a07ae58e224e088a76dc4d0ac8577c598edbf04
Wendel de Oliveira Santos <wendel.oliveira.santos@outlook.com>
2024-08-20 17:36:40 -0300

    dsd

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { AdminModule } from './modules/admin/admin.module';
       load: [appConfig, universalAssistanceTravelConfig, assistCardConfig, nowSysConfig],
       envFilePath: ['.env'],
     }),
-    MongooseModule.forRoot('mongodb://root:p1Hh7P0juRB0OIsZlxfyaHZwq9ncOqGOCJwggxyOzkwXYXsfT4xmAstzmp9kjL42@92.113.33.10:5432?directConnection=true'),
+    MongooseModule.forRoot('mongodb://root:p1Hh7P0juRB0OIsZlxfyaHZwq9ncOqGOCJwggxyOzkwXYXsfT4xmAstzmp9kjL42@92.113.33.10:5432/global?authSource=admin&directConnection=true'),
     UniversalAssistanceModule,
     AssistCardModule,
     NowSysModule,
