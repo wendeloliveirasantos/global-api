199a3762f4df8ab298c868b1be466bd936688c08
Wendel de Oliveira Santos <wendel.oliveira.santos@outlook.com>
2024-08-21 14:34:59 -0300

    ww

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,6 +21,7 @@ import { AdminModule } from './modules/admin/admin.module';
       isGlobal: true,
       load: [appConfig, universalAssistanceTravelConfig, assistCardConfig, nowSysConfig],
       envFilePath: ['.env'],
+      ignoreEnvFile: false,
     }),
     MongooseModule.forRoot('mongodb://root:p1Hh7P0juRB0OIsZlxfyaHZwq9ncOqGOCJwggxyOzkwXYXsfT4xmAstzmp9kjL42@92.113.33.10:5432/global?authSource=admin&directConnection=true'),
     UniversalAssistanceModule,
